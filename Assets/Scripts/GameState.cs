e056beeffdab8c29226fd273179d395cfde67327
Chris Trott <christrott86@gmail.com>
2018-11-19 07:44:58 +1100

    Added main menu

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum States
+{
+    PLAYING,
+    GOAL,
+    RUSH,
+    OVERTIME
+}
+
+public class GameState : MonoBehaviour {
+    States gameState;
+    private float _timer;
+
+    public float Timer
+    {
+        get {
+            return _timer;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        gameState = States.PLAYING;
+	}
+	
+	public void UpdateState(States newState)
+    {
+        switch (newState)
+        {
+            case States.PLAYING:
+                // Resume Timer
+                break;
+            case States.GOAL:
+                ResetPlayState();
+                break;
+        }
+        gameState = newState;
+    }
+
+    private void ResetPlayState()
+    {
+
+    }
+}
---
+++ Assets/Scripts/UI/Button.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour {
+    public void OpenScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
