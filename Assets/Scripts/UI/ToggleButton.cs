1fb86627554a011cd1877cba182ed6e3b9532664
Chris Trott <christrott86@gmail.com>
2018-11-30 17:36:07 +1100

    Added music and sound mute buttons on main menu

---
+++ Assets/Scripts/UI/ToggleButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class ToggleButton : MonoBehaviour {
+    public AudioMixer mixer;
+
+    private bool muted;
+    private Image mutedImage;
+
+	// Use this for initialization
+	void Start () {
+        muted = false;
+        mutedImage = transform.GetChild(0).GetComponent<Image>();
+        mutedImage.enabled = false;
+    }
+
+    public void ToggleAudioGroup(string paramName)
+    {
+        Debug.Log(paramName + ": " + muted);
+        if (muted)
+        {
+            mixer.SetFloat(paramName, 0.0f);
+            muted = false;
+            mutedImage.enabled = false;
+        }
+        else
+        {
+            mixer.SetFloat(paramName, -80.0f);
+            muted = true;
+            mutedImage.enabled = true;
+        }
+    }
+}
