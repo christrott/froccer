a2e82a1c754f1b461dd8b76f3bcf35c277fff143
Chris Trott <christrott86@gmail.com>
2018-12-01 23:28:42 +1100

    Fixed an issue with pause menu quit to main option

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -7,6 +7,7 @@ public class PauseMenu : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        Time.timeScale = 1.0f;
         menuContainer = transform.GetChild(0).gameObject;
 	}
 	
